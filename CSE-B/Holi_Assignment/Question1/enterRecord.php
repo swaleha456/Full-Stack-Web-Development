eb7863b947f209f63c8bdf4301e29f208a5e1bef
swaleha <swaleha2612000@gmail.com>
2020-03-24 16:16:23 +0530

    Answer 1

---
+++ CSE-B/Holi_Assignment/Question1/enterRecord.php
@@ -0,0 +1,20 @@
+
+ <!DOCTYPE html>
+<html>
+    <head>
+    <title>Input Form</title>
+    </head>
+    
+    <body>
+    <form action="enterRecord.php" method="post">
+      Enrollment No.:<input type="text" name="enrollment" placeholder="Enter Enrollment no."><br>
+      Name: <input type="text" name="username"><br>
+      Age:<input type="text" name="age" placeholder="Enter Age"><br>
+      Course:<input type="text" name="course" placeholder="Enter Course"><br>
+      Branch:<input type="text" name="branch" placeholder="Enter Branch"><br>
+      Contact:<input type="text" name="contact" placeholder="Enter Contact"><br>
+      Email: <input type="email" name="email" placeholder="Enter Email"><br>
+      <input type="submit" name='btn'>
+    </form>
+    </body>
+</html>
